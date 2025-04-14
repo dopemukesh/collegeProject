cbcfa897654e7ae6e5c3a0943a88ad0d237e7db8
dopemukesh <dope.mukeshyadav@gmail.com>
2025-04-14 22:20:47 +0530

    Update BASE_URL configuration for local and production environments

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
 
 
 // conditional type 
-// const BASE_URL = window.location.hostname === 'localhost'
-//     ? 'http://localhost:3000' // local backend URL
-//     : 'https://your-backend.onrender.com'; // actual backend URL aayega
+const BASE_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:3000' // local backend URL
+    : 'https://collegeproject-5vpp.onrender.com'; // actual backend URL aayega
